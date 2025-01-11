cc4a35d79d169643e14adc52882d33a4d974920d
F1eshka <144143888+F1eshka@users.noreply.github.com>
2025-01-11 12:14:33 +0300

    Add files via upload

---
+++ Focus.HW/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Focus.HW
+{
+    public partial class MainWindow : Window
+    {
+        public static readonly RoutedCommand ChangeColorCommand = new RoutedCommand();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            CommandBinding colorCommandBinding = new CommandBinding(ChangeColorCommand, ChangeColorExecuted);
+            this.CommandBindings.Add(colorCommandBinding);
+            KeyGesture keyGesture = new KeyGesture(Key.R, ModifierKeys.Control);
+            InputBinding inputBinding = new InputBinding(ChangeColorCommand, keyGesture);
+            this.InputBindings.Add(inputBinding);
+        }
+
+        private void ChangeColorExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            SetFocusedControlColor();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SetFocusedControlColor();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            SetFocusedControlColor();
+        }
+
+        private void SetFocusedControlColor()
+        {
+            if (Keyboard.FocusedElement is Control focusedControl)
+            {
+                focusedControl.Background = Brushes.Red;
+            }
+        }
+    }
+}
